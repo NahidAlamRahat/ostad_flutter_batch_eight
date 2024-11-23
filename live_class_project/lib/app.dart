2b5ffbb72c82b9c68bf1a72daf1193ce7e831f80
RafatMeraz <razhossain201@gmail.com>
2024-11-23 23:13:22 +0600

    Add todo list ui

---
+++ live_class_project/lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:live_class_project/screens/todo_list_screen.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TodoListScreen(),
+    );
+  }
+}
